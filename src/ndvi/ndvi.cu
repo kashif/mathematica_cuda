a5d7b071cbc8373672a55aac1f04ccaa8ca78454
Shoaib Burq <saburq@gmail.com>
2009-09-08 08:24:46 +1000

    added ndvi from Raq

---
+++ src/ndvi/ndvi.cu
@@ -0,0 +1,96 @@
+/*
+ * This sample calculates Normalized Vegitation Index (NDVI) of a 
+ * given pair of near infra red and red
+ */
+
+#include <stdio.h>
+#include <cutil_inline.h>
+#include <mathlink.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Calculate NDVI on CPU
+///////////////////////////////////////////////////////////////////////////////
+extern "C"
+void ndvi(void);
+
+///////////////////////////////////////////////////////////////////////////////
+// Calculate NDVI on GPU
+///////////////////////////////////////////////////////////////////////////////
+#include "ndvi_kernel.cu"
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Main program
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{   
+    // use command-line specified CUDA device, otherwise use device with highest Gflops/s
+    if( cutCheckCmdLineFlag(argc, (const char**)argv, "device") )
+        cutilDeviceInit(argc, argv);
+    else
+        cudaSetDevice( cutGetMaxGflopsDeviceId() );
+
+    int result = MLMain(argc, argv);
+    cutilExit(argc, argv);
+    return result;
+}
+
+void ndvi(void)
+{
+    short int *h_A, *h_B;
+    float *h_C_GPU;
+    short int *d_A, *d_B;
+    float *d_C;
+
+    char **heads_A, **heads_B;
+    int *dims_A, *dims_B;
+    int rank_A, rank_B;
+
+    if(! MLGetInteger16Array(stdlink, &h_A, &dims_A, &heads_A, &rank_A))
+    {
+        return;
+    }
+    
+    if(! MLGetInteger16Array(stdlink, &h_B, &dims_B, &heads_B, &rank_B))
+    {
+        return;
+    }
+    
+    //Initializing data
+    h_C_GPU = (float *)malloc(dims_A[0]*dims_A[1]*sizeof(float));
+
+    //Allocating GPU memory
+    cutilSafeCall( cudaMalloc((void **)&d_A, dims_A[0]*dims_A[1]*sizeof(short int)) );
+	//printf("array size %d\n", dims_A[0]*dims_A[1]*sizeof(short int));
+    cutilSafeCall( cudaMalloc((void **)&d_B, dims_A[0]*dims_A[1]*sizeof(short int)) );
+    cutilSafeCall( cudaMalloc((void **)&d_C, dims_A[0]*dims_A[1]*sizeof(float)) );
+
+    //Copy options data to GPU memory for further processing 
+    cutilSafeCall( cudaMemcpy(d_A, h_A, dims_A[0]*dims_A[1]*sizeof(short int),cudaMemcpyHostToDevice) );
+    cutilSafeCall( cudaMemcpy(d_B, h_B, dims_A[0]*dims_A[1]*sizeof(short int),cudaMemcpyHostToDevice) );
+
+    cutilSafeCall( cudaThreadSynchronize() );
+
+    dim3 grid(ceil((float)dims_A[0]/(float)16.0f), ceil((float) dims_A[1]/32.0f), 1);
+    dim3 threads(ceil( dims_A[0]/(float)grid.x), ceil( dims_A[1]/(float)grid.y), 1);
+
+
+    ndviGPU<<<grid, threads>>>(d_C, d_A, d_B, dims_A[0], dims_A[1]);
+    cutilCheckMsg("ndviGPU() execution failed\n");
+    cutilSafeCall( cudaThreadSynchronize() );
+
+    //Read back GPU results to compare them to CPU results
+    cutilSafeCall( cudaMemcpy(h_C_GPU, d_C, dims_A[0]*dims_A[1]*sizeof(float), cudaMemcpyDeviceToHost) );
+
+    MLPutReal32List(stdlink, h_C_GPU, dims_A[0]*dims_A[1]);
+
+    cutilSafeCall( cudaFree(d_C) );
+    cutilSafeCall( cudaFree(d_B) );
+    cutilSafeCall( cudaFree(d_A) );
+
+    MLReleaseInteger16Array(stdlink, h_A, dims_A, heads_A, rank_A);
+    MLReleaseInteger16Array(stdlink, h_B, dims_B, heads_B, rank_B);
+
+    cudaThreadExit();
+}
+
---
+++ src/ndvi/ndvi.tm
@@ -0,0 +1,7 @@
+:Begin:
+:Function:      ndvi
+:Pattern:       ndvi[ a_List, b_List ]
+:Arguments:     { a, b }
+:ArgumentTypes: { Manual }
+:ReturnType:    Manual
+:End:
